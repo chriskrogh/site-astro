0519de44a39f750d788b063bb4366c39e3ac88b4
Christopher Krogh <chris.krogh@outlook.com>
2023-02-05 19:14:02 -0500

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
 import { defineConfig } from "astro/config";
 // https://astro.build/config
 import vanillaExtract from "astro-vanilla-extract";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vanillaExtract()],
+  integrations: [
+    vanillaExtract(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
