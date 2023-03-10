6a7aaff9cb63dfe07bb9fe9927630419a10a1376
Christopher Krogh <chris.krogh@outlook.com>
2023-03-09 23:54:13 -0500

    content collection config

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+import { defineCollection, z } from "astro:content";
+
+const experiences = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    image: z.string(),
+    link: z.string(),
+    positions: z.array(
+      z.object({
+        title: z.string(),
+        achievements: z.array(z.string()),
+        duration: z.object({
+          start: z.string(),
+          end: z.string().optional(),
+        }),
+      }),
+    ),
+    tech: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  experiences,
+};
